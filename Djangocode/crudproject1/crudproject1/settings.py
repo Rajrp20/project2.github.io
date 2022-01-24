705560e2500a04a1da4cc079247379da32a0d60e
Rajrp20 <91689114+Rajrp20@users.noreply.github.com>
2022-01-24 23:18:13 +0530

    Update settings.py

--- Djangocode/crudproject1/crudproject1/settings.py
+++ Djangocode/crudproject1/crudproject1/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*464-apc8ci#e+gtkf+j--6_2#m&jb=2y@-ef_evicrf3++@d0'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
